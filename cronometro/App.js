d4bb6d458cf22343f980e120f0673ef24f20f36c
BMateusSs <bmdss0810@gmail.com>
2025-05-22 17:03:08 -0300

    Implementando a lógica do cronômetro

--- cronometro/App.js
+++ cronometro/App.js
@@ -3,22 +3,42 @@ import { StyleSheet, Text, View } from 'react-native';
 import Imagem from './components/Imagem';
 import Botao from './components/Botao';
 import Timer from './components/Timer';
+import { useRef, useState } from 'react';
 
 export default function App() {
+  const [numero, setNumero] = useState(0);
+  const [ativo, setAtivo] = useState(false);
+  const intervalRef = useRef(null);
+
+  function getIniciar(){
+    if (ativo) return;
+
+    setAtivo(true);
+    intervalRef.current = setInterval(() => {setNumero((anterior) => anterior + 0.1);}, 100 )
+  }
+
+  function getPausar(){
+    clearInterval(intervalRef.current)
+    setAtivo(false)
+    setNumero(0)
+  }
+
   return (
     <View style={styles.container}>
       <Imagem/>
 
       <Timer
-      texto="0.0"
+      texto={numero.toFixed(1)}
       />
       <View style={styles.containerBotao}>
         <Botao
       texto="Iniciar"
+      onPress={getIniciar}
       />
 
       <Botao
       texto="Limpar"
+      onPress={getPausar}
       />
       </View>
       
