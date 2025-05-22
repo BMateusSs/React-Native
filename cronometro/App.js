542b2008b090e9dd432b165246043fef09e0f872
BMateusSs <bmdss0810@gmail.com>
2025-05-22 16:15:42 -0300

    Componente timer

--- cronometro/App.js
+++ cronometro/App.js
@@ -2,11 +2,16 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Imagem from './components/Imagem';
 import Botao from './components/Botao';
+import Timer from './components/Timer';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Imagem/>
+
+      <Timer
+      texto="0.0"
+      />
 
       <Botao
       texto="Iniciar"
---
+++ cronometro/components/Timer.js
@@ -0,0 +1,15 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+const Timer = ({texto}) => {
+    return(
+        <View>
+            <Text>{texto}</Text>
+        </View>
+    )
+}
+
+export default Timer;
+
+const style = StyleSheet.create({
+
+})
