18866fad02eabacaa09d13761b01f3d907213204
BMateusSs <bmdss0810@gmail.com>
2025-05-25 12:14:20 -0300

    Componente input

---
+++ calcular-compensacao/components/Input.js
@@ -0,0 +1,24 @@
+import { View, TextInput, StyleSheet } from "react-native";
+
+const Input = ({value, onChange}) => {
+    return(
+        <View style={styles.input}>
+            <TextInput
+            placeholder="Ex: 4.60"
+            value={value}
+            onChangeText={onChange}
+            />
+        </View>
+    )
+}
+
+export default Input;
+
+const styles = StyleSheet.create({
+    input: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+        width: '80%',
+        marginBottom: 20
+    }
+})
