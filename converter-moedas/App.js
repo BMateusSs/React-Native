6ad1d097a0005b06ce516763e4231d9e32bf4453
BMateusSs <bmdss0810@gmail.com>
2025-05-26 16:59:06 -0300

    Implementando api

--- converter-moedas/App.js
+++ converter-moedas/App.js
@@ -1,9 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import {useEffect, useState} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 import AreaMoeda from './components/AreaMoeda';
+import { api } from './services/api';
+
 
 export default function App() {
+  const [moedas, setMoedas] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadMoedas() {
+      const response = await api.get('all')
+
+      let arrayMoedas = [];
+      Object.keys(response.data).map((key) => {
+        arrayMoedas.push({
+          key: key,
+          label: key,
+          value: key
+        }
+        )
+      })
+      setMoedas(arrayMoedas)
+      setLoading(false)
+
+    }
+    loadMoedas();
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.subContainer}>
