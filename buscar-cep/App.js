127a4f70b039766e23268215de9a04729d532917
BMateusSs <bmdss0810@gmail.com>
2025-05-30 17:31:45 -0300

    Primeiros componentes

--- buscar-cep/App.js
+++ buscar-cep/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
 
 export default function App() {
+  const [cep, setCep] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{justifyContent: 'center'}}>
+        <Text>Digite o CEP desejado:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Ex: 12345678'
+          value={cep}
+          onChangeText={setCep}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
