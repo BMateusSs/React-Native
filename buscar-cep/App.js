5c0fb44ed2ce223afb9e200e4032c408f96ed26a
BMateusSs <bmdss0810@gmail.com>
2025-05-31 14:35:29 -0300

    Botôes

--- buscar-cep/App.js
+++ buscar-cep/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
-import api from './services/api';
+import api from './services/api'
 
 export default function App() {
   const [cep, setCep] = useState('')
 
+  async function buscar(){
+
+  }
+
   return (
     <View style={styles.container}>
-      <View style={{alignItems: 'center'}}>
+      <View style={{alignItems: 'center', width: '100%'}}>
         <Text style={styles.text}>Digite o CEP desejado:</Text>
         <TextInput
           style={styles.input}
@@ -16,6 +20,18 @@ export default function App() {
           value={cep}
           onChangeText={setCep}
         />
+      </View>
+
+      <View style={styles.areabtn}>
+        <TouchableOpacity style={[styles.botao, {backgroundColor: 'green'}]}>
+          <Text style={styles.textbtn}>Buscar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.botao, {backgroundColor: 'red'}]}
+        onPress={buscar}
+        >
+          <Text style={styles.textbtn}>Limpar</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -42,5 +58,23 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     fontWeight: 'bold',
     fontSize: 25,
+  },
+  areabtn: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 15,
+    width: '100%'
+  },
+  botao:{
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    padding: 15
+  },
+  textbtn: {
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 });
--- buscar-cep/services/api.js
+++ buscar-cep/services/api.js
@@ -1,1 +1,7 @@
+import axios from 'axios'
 
+const api = axios.create({
+    baseURL: 'https://viacep.com.br/ws'
+});
+
+export default api;
